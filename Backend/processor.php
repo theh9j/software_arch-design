fc0a331f99b24bf180db732902a085bcff1b60a2
Pham Quang Minh <pminh12012005@gmail.com>
2025-07-02 16:58:55 +0700

    Processor build

--- Backend/processor.php
+++ Backend/processor.php
@@ -1,33 +1,97 @@
 <?php
 session_start();
-require_once("Backend/constants.php");
 
-class Account {
-    private $name;
-    private $age;
-    private $address;
+abstract class Account {
+    private string $name;
+    private string $dob;
+    private string $address = "Not defined";
 
-    function Account() {
-        $address = "Not defined";
+    public function __construct($name, $dob) {
+        $this->name = $name;
+        $this->dob = $dob;
     }
 
-    function updateAddress($newAddress) {
-        $address = $newAddress;
-        return $address;
+    public function getAddress() {
+        return $this->address;
     }
 
-    function updateName($newName) {
-        $name = $newName;
-        return $name;
+    public function getName() {
+        return $this->name;
     }
 
-    function updateAge($newAge) {
-        
+    public function updateAddress($newAddress) {
+        $this->address = $newAddress;
+    }
 
-        return $age;
+    public function updateName($newName) {
+        $this->name = $newName;
+    }
+
+    abstract public function updateDob($newDob);
+}
+
+class User extends Account {
+    private int $changeCount = 0;
+    private string $uniqid;
+
+    public function __construct($name, $dob) {
+        parent::__construct($name, $dob);
+        $this->dob = $dob;
+        $this->uniqid = UniqueID();
+        $_SESSION["id"] = $this->uniqid;
+    }
+
+    public function getID() {
+        return $this->uniqid;
+    }
+
+    public function getDob() {
+        return $this->dob;
+    }
+
+    public function updateDob($newDob) {
+        if ($changeCount != 2) {
+            $changeCount += 1;
+            $this->dob = $newDob;
+            return "Date of Birth updated!";
+        } else {
+            return "Date of Birth has been changed too many times!";
+        }
     }
 }
 
+class Manager extends Account {
 
 
+    public function __constract() {
+
+    }
+}
+
+function UniqueID($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+}
+
+function Processor() {
+    $users = [];
+    $users[] = new User("James", "12-22-2004");
+    $users[] = new User("Alice", "09-02-2000");
+
+    foreach ($users as $user) {
+        if ($user->getID() === $_SESSION['id']) {
+            $name = $user->getName();
+            echo "Hello, $name";
+        }
+    }
+}
+
+Processor();
 ?>
